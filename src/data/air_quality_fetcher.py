df3ca04ec940b776d75324e0e64582ba1fc66620
sharjeel435 <sharjeel435@users.noreply.github.com>
2026-08-27 15:45:12 +0500

    Report and Test of Project

--- src/data/air_quality_fetcher.py
+++
@@ -1,5 +0,0 @@
-from config.settings import AIR_QUALITY_VARIABLES
-
-
-def fetch_air_quality(client, city, start, end):
-    return client.fetch_historical(city, start, end, AIR_QUALITY_VARIABLES, "air_quality")
--- src/data/weather_fetcher.py
+++
@@ -1,5 +0,0 @@
-from config.settings import WEATHER_VARIABLES
-
-
-def fetch_weather(client, city, start, end):
-    return client.fetch_historical(city, start, end, WEATHER_VARIABLES, "weather")
